7a8a54de981b50db77dcfef4e564c7695ffb43ce
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-11 13:05:00 +0700

    sorting data asc dan desc

---
+++ sorting data asc dan desc/pengguna.py
@@ -0,0 +1,34 @@
+from peewee import *
+import random, datetime
+
+sqlite_db = SqliteDatabase('pengguna.db')
+
+class Pengguna(Model):
+    username = CharField()
+    point = IntegerField()
+    join_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = sqlite_db
+
+sqlite_db.connect()
+sqlite_db.create_tables([Pengguna], safe=True)
+
+users = Pengguna.select().order_by(Pengguna.point.desc())
+
+for user in users:
+    print(user.username) + ' ' + str(user.point)
+
+'''
+def get_rand():
+    return random.randint(1,100)
+
+data = [
+    {'username': 'yudhi', 'point': get_rand()},
+    {'username': 'rangga', 'point': get_rand()},
+    {'username': 'boy', 'point': get_rand()},
+    {'username': 'masta', 'point': get_rand()}
+]
+
+Pengguna.insert_many(data).execute()
+'''
